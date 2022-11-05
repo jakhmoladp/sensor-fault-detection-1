178ae492db9e1704271b1c34dbe4d887739128d6
Devendra Prasad Jakhmola <devjakhmola1990@gmail.com>
2022-11-05 11:22:59 +0530

    train-test splits created

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -1,55 +1,82 @@
-from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
-import sys, os
+from sklearn.model_selection import train_test_split
+import os,sys
 from pandas import DataFrame
 from sensor.data_access.sensor_data import SensorData
-
+from sensor.utils.main_utils import read_yaml_file
+from sensor.constant.training_pipeline import SCHEMA_FILE_PATH
 class DataIngestion:
 
-    def __init__(self, data_ingestion_config: DataIngestionConfig):
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
             self.data_ingestion_config=data_ingestion_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
         except Exception as e:
-            raise SensorException(e, sys)
+            raise SensorException(e,sys)
 
     def export_data_into_feature_store(self) -> DataFrame:
         """
-        Import mongodb collection record as dataframe
+        Export mongo db collection record as data frame into feature
         """
         try:
-            logging.info("Importing Data from MongoDB to DataFrame")
-            # initialize the sensor data class (mongo db connection attributes)
+            logging.info("Exporting data from mongodb to feature store")
             sensor_data = SensorData()
-            # import the data from the required collection (table) from mongo db
             dataframe = sensor_data.export_collection_as_dataframe(collection_name=self.data_ingestion_config.collection_name)
-            # once data is imported, we need to save it.
-            # we'll save the feature data in the feature store directory
-            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
-            print("Feature Store File Path: --> ", feature_store_file_path)
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path            
 
-            # Create folders
+            #creating folder
             dir_path = os.path.dirname(feature_store_file_path)
-            os.makedirs(dir_path, exist_ok=True)
-
-            dataframe.to_csv(feature_store_file_path, index=False, header=True)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
             return dataframe
-
-
-        except Exception as e:
-            raise SensorException(e, sys)
+        except  Exception as e:
+            raise  SensorException(e,sys)
 
     def split_data_as_train_test(self, dataframe: DataFrame) -> None:
         """
-        Split the feature store data into train and test sets
+        Feature store dataset will be split into train and test file
         """
-        pass
+
+        try:
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
+
+            logging.info("Performed train test split on the dataframe")
+
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+
+            os.makedirs(dir_path, exist_ok=True)
+
+            logging.info(f"Exporting train and test file path.")
+
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
+
+            logging.info(f"Exported train and test file path.")
+        except Exception as e:
+            raise SensorData(e,sys)
     
+
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
             dataframe = self.export_data_into_feature_store()
-            self.split_data_as_train_test(dataframe)
+            dataframe = dataframe.drop(self._schema_config["drop_columns"],axis=1)
+            self.split_data_as_train_test(dataframe=dataframe)
+            data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+            test_file_path=self.data_ingestion_config.testing_file_path)
+            return data_ingestion_artifact
         except Exception as e:
-            raise SensorException(e, sys)
+            raise SensorException(e,sys)
